b7404c158dbf7f49b9afa1a303214cdf9f0b08ce
shreyas0906 <shreyas0906@gmail.com>
2022-01-11 14:22:30 +0530

    updates from 10/1/2022

--- simple_fcn.py
+++ simple_fcn.py
@@ -1,4 +1,5 @@
 # Imports
+import sys
 import torch
 import torch.nn.functional as F  # Parameterless functions, like (some) activation functions
 import torchvision.datasets as datasets  # Standard datasets
@@ -24,6 +25,28 @@ class NN(nn.Module):
         return x
 
 
+class CNN(nn.Module):
+
+    def __init__(self, in_channels=1, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.fc1 = nn.Linear(16 * 7 * 7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+
+        return x
+
+model = CNN()
+x = torch.randn(64, 1, 28, 28)
+# print(model(x).shape)
 # model = NN(784, 10)
 # x = torch.randn(64, 784)
 # print(model(x).shape)
@@ -34,11 +57,13 @@ print(f"Device: {str(device).upper()}")
 
 
 # Hyper parameters
+in_channel = 1
 input_size = 784
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
 num_epochs = 10
+load_model = True
 
 #load data
 train_dataset = datasets.MNIST(root='MNIST', train=True, transform=transforms.ToTensor(), download=True)
@@ -48,7 +73,8 @@ test_dataset = datasets.MNIST(root='MNIST', train=False, transform=transforms.To
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 #initialize network
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+# model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN().to(device)
 
 #Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -56,19 +82,40 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train network
 
+
+def save_checkpoint(state, filename='saved_model.pth.tar'):
+    print(f"saving model")
+    torch.save(state, filename)
+
+
+def load_checkpoint(checkpoint):
+    print(f"loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+
+if load_model:
+    load_checkpoint(torch.load('saved_model.pth.tar'))
+    print(f"loading saved model....")
+
+
 for epoch in tqdm(range(num_epochs), desc=f'training'):
-    # print(f"Epoch number: {epoch}")
+
+    checkpoint = {'state_dict': model.state_dict(), "optimizer": optimizer.state_dict()}
+    if epoch != 0 and epoch % 2 == 0:
+        save_checkpoint(checkpoint)
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device)
         targets = targets.to(device=device)
 
         # reshape the data from ([64, 1, 28, 28]) --> ([64, 784])
-        data = data.reshape(data.shape[0], -1)
+        # data = data.reshape(data.shape[0], -1)
         # print(f"data shape: {data.shape}    target: {targets.shape}")
 
         # Forward
         scores = model(data)
         loss = criterion(scores, targets)
+
 
         # Backward
         optimizer.zero_grad()
@@ -78,6 +125,7 @@ for epoch in tqdm(range(num_epochs), desc=f'training'):
         optimizer.step()
 
         # print(f"loss: {loss}")
+    print(f"\nloss at epoch: {epoch} is {loss:.3f}")
 
 
 #check accuracy on training and test
@@ -97,7 +145,7 @@ def check_accuracy(loader, model):
             x = x.to(device)
             y = y.to(device)
 
-            x = x.reshape(x.shape[0], -1)
+            # x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
